daf6df6f6d44c972d7546576c9a2ab6767c88cf5
Aryan <72085890+Aryan-Sheregar@users.noreply.github.com>
2025-09-21 01:39:27 +0530

    Added web crawl tavily

--- agent_tools.py
+++ agent_tools.py
@@ -2,6 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 import fitz
 from pymupdf4llm import to_markdown
+from langchain_tavily import TavilySearch
+
+def get_web_search_results():
+    return TavilySearch(max_results=5)
+
 def scrape_website(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
